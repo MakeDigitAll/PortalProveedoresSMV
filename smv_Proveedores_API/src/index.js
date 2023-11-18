25137bbaf07730db33942d1936b42ff5dd024cd9
JuanDiegodeAvila <diego@makedigitall.com>
2023-11-17 20:56:17 -0600

    I

--- smv_Proveedores_API/src/index.js
+++ smv_Proveedores_API/src/index.js
@@ -15,7 +15,7 @@ const authRoutes = require('./routes/auth.routes');
 const usersRoutes = require('./routes/users.routes');
 const productsRoutes = require('./routes/products.routes');
 const orders = require('./routes/orders.routes');
-const invoices = require('./routes/invoices.routes');
+const invoices = require('./routes/Invoices.routes');
 
 //configuraciones
 //app.use(logger);
